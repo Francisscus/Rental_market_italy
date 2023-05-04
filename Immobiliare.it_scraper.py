ad637aa4d66448d294b7c3b4d2e6c2590b416df6
Francisscus <132554535+Francisscus@users.noreply.github.com>
2023-05-04 10:48:12 +0200

    Update Immobiliare.it_scraper.py

--- Immobiliare.it_scraper.py
+++ Immobiliare.it_scraper.py
@@ -24,7 +24,7 @@ from selenium.webdriver.support import expected_conditions as EC
 # - csv file name: number_rent_city.csv
 # - the number put in the csv file title is used just to order the 20 files
 
-target_url = 'https://www.immobiliare.it/affitto-case/taranto/?criterio=rilevanza'
+target_url = 'https://www.immobiliare.it/affitto-case/milano/?criterio=rilevanza'
 
 # this few lines allow you to install the driver only when you use the script instead of download and update it everytime
 chrome_driver = ChromeDriverManager().install()
@@ -38,30 +38,30 @@ print(f'Start_time {start_time}')
 with open('19_rent_taranto.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    #refuse cookies
+    # refuse cookies
     driver.find_element(By.CSS_SELECTOR, "span.didomi-continue-without-agreeing").click()
     sleep(2)
-    #refuse the option to saved the research
+    # refuse the option to saved the research
     try:
         driver.find_element(By.CSS_SELECTOR, "button.nd-dialogFrame__close").click()
     except:
         pass
 
-    # below there the information I collected for each rental ad
+    # below, there is the information I collected from each rental ad
     header = ["Index", "Title description", "Price", "N_rooms", "Surface", "N_toilettes", "Floor"]
     writer.writerow(header)
 
-    # the number of pages to scrape may vary from city to city,
-    # therefore it is necessary to change the index between square brackets to find the right css selector
+    # the number of pages to scrape might vary from city to city,
+    # therefore, it is necessary to change the index between square brackets to find the right css selector
     # remember that find_elements is plural because it takes identical css selectors and store them in a list
     last_page_number = int((driver.find_elements(By.CSS_SELECTOR, ".in-pagination__item.hideOnMobile")[3]).text)
 
     num = 0
     for num_page in range(1, last_page_number+1):
         print(f'Starting page: {num_page}')
-        driver.implicitly_wait(3) #this wait allows to load a new page, otherwise we cannot scraping anything
+        driver.implicitly_wait(3) #this wait allows to load a new page, otherwise we cannot scrape anything
 
-        # the following line is necessary to find the box of the ad that contains all elements of each rent in the web page
+        # the following line is necessary to find the box of the ad that contains all the elements of each rent in the web page
         houses = driver.find_elements(By.CSS_SELECTOR,
         ".in-card.nd-mediaObject.nd-mediaObject--colToRow.in-realEstateCard.in-realEstateCard--interactive.in-realEstateListCard")
 
@@ -115,7 +115,7 @@ with open('19_rent_taranto.csv', 'w', newline='', encoding='utf-8') as file:
         execution_time = round((time() - start_time) / 60, 2)
         print(f'Execution time : {execution_time} minutes')
 
-        #the followig lines are used to find the next page and click on it until the last one
+        #the following lines are used to find the next page and click on it until the last one
         if num_page == 1:
            WebDriverWait(driver, 1).until(
                 EC.visibility_of_element_located((By.CSS_SELECTOR, "a[class$='in-pagination__item']"))
@@ -126,4 +126,4 @@ with open('19_rent_taranto.csv', 'w', newline='', encoding='utf-8') as file:
             new_page.click()
 
 
-driver.quit()
+driver.quit() # when we arrive to the last page, this line closes the program
